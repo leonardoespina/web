11d2a9b66cb352de85ed2736b827b74ba5cb2e40
leonardoespina <leonardoespina@hotmail.com>
2025-04-23 22:44:43 -0400

    cambio de ruta assest y cart se agrego opcion ws

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "66199953"
+    serviceWorkerVersion: "2239845904"
   }
 });
